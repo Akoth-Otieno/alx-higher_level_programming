c8df967d420263dd5ac15b06888df177a10d4bc8
AKoth-Otieno <elsyakoth@gmail.com>
2022-08-24 07:10:26 -0700

    Correct syntax error on line 12.

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,10 +5,10 @@ def fizzbuzz():
         if i % 3 == 0 and i % 5 == 0:
              print("FizzBuzz ", end="")
 
-        elif i % 3 == 0:
+        if i % 3 == 0:
             print(print("Fizz ", end="")
 
-        elif i % 5 == 0:
+        if i % 5 == 0:
             print("Buzz ", end="")
 
         else:
