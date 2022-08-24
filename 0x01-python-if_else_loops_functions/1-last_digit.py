214fea6f83788add80697710c62d12d47e4ccb26
AKoth-Otieno <elsyakoth@gmail.com>
2022-08-24 02:39:23 -0700

    Correct syntax error in the fstrings.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -15,7 +15,7 @@ elif (last_digit == 0):
     print(f"Last digit of {number} is {last_digit} and is 0")
     
 else:
-    print(f"Last digit of {number} is {last_digit} and and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
 
 
 
