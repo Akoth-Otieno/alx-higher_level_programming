1e2203b0e3d2d17a3fc432e72d1beec67056049e
AKoth-Otieno <elsyakoth@gmail.com>
2022-08-25 12:00:46 -0700

    Define the function that adds two numbers.

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Fuxntion that adds two numbers.
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The sum of a + b
+    """
+    return (a + b)
