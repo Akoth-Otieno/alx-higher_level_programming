3454bfd3467fe570cb56b495f08e6c21e307abb2
AKoth-Otieno <elsyakoth@gmail.com>
2022-08-25 11:54:07 -0700

    Write a program that imports the function def add(a, b): from the file add_0.py.

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    '''Program that imports the function def add(a,b):'''
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
