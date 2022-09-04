762058b12de190a09bab70520c05d5c83b3a0d05
AKoth-Otieno <elsyakoth@gmail.com>
2022-09-04 00:05:33 -0700

    remove unnecessary statements.

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,6 @@ if __name__ == "__main__":
     """print the sum of all arguments"""
     import sys
 
-    args_count = len(sys.argv)
     sums = 0
     for i in range(len(sys.argv) - 1):
         sums += int(sys.argv[i + 1])
