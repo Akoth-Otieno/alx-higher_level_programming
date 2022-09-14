c42e504cdd210f2a82431ca1c575b3863d1092ac
AKoth-Otieno <elsyakoth@gmail.com>
2022-09-14 10:12:01 -0700

    define a public instance method that returns the area of a square.

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Define a class square."""
+
+
+class Square:
+    """Initialize size as private instance attribute"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >=0")
+        self.__size = size
+
+    def area(self):
+        """Define public instance method that returns area"""
+        return (self.__size * self.size)
