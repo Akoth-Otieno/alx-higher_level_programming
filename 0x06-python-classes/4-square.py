4c515d4c11dec726d69bc5aa1c97e2483ab98a90
AKoth-Otieno <elsyakoth@gmail.com>
2022-09-14 13:06:09 -0700

    define getter and setter property methods.

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""Write a class Square."""
+
+
+class Square:
+    """Define and initialize size as a private instance attribute."""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Define public instance method that returns area."""
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
