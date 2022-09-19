180bf4976b26201e57483f8981b15a8f1832592e
AKoth-Otieno <elsyakoth@gmail.com>
2022-09-19 12:14:38 -0700

    draw a rectangle.

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
-'''Write a class Rectangle that defines a rectangle.'''
+"""Define the square class"""
 
 
 class Rectangle:
-    '''Define class Rectangle.'''
+    """Define class rectangle"""
     def __init__(self, width=0, height=0):
-        '''Create a new rectangle.'''
         if type(width) != int:
             raise TypeError("width must be an integer")
         if width < 0:
@@ -17,20 +16,6 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
-
-    @property
-    '''get width of new rectangle.'''
-    def width(self):
-        return(self.__width)
-
-    @width.setter
-    def width(self, value):
-        '''set width of new rectangle'''
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
 
     @property
     def width(self):
@@ -65,12 +50,12 @@ class Rectangle:
         return 2 * self.__width + 2 * self.__height
 
     def __str__(self):
-        rect = ""
+        rectangle = ""
         if self.__width == 0 or self.__height == 0:
-            return (rect)
+            return (rectangle)
         for i in range(0, self.__height):
             for j in range(self.__width):
-                rect += "#"
+                rectangle += "#"
             if i is not (self.__height - 1):
-                rect += '\n'
-        return (rect)
+                rectangle += '\n'
+        return (rectangle)
