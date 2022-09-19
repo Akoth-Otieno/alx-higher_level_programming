29b851ed58d107d4dbc24c3aae2adabcdb6a6787
AKoth-Otieno <elsyakoth@gmail.com>
2022-09-19 11:40:59 -0700

    rewrite logic.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,20 +4,25 @@
 
 
 class Rectangle:
-    '''Define class Rectangle.'''
+    """class rectangule empty"""
     def __init__(self, width=0, height=0):
-        '''Create a new rectangle.'''
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
     @property
-    '''get width of new rectangle.'''
     def width(self):
-        return(self.__width)
+        return self.__width
 
     @width.setter
     def width(self, value):
-        '''set width of new rectangle'''
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -26,12 +31,10 @@ class Rectangle:
 
     @property
     def height(self):
-        '''retrieve height of new rectangle.'''
-        return(self.__height)
+        return self.__height
 
     @height.setter
-    '''set height of new rectangle.'''
-    def width(self, value):
+    def height(self, value):
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
