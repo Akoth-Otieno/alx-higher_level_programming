6e7e7e2d75319eaf2e2dd3ee70c671c91fc9a258
AKoth-Otieno <elsyakoth@gmail.com>
2022-09-29 01:38:54 -0700

    remove white spaces.

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,7 +2,7 @@
 
 '''Define Function that list of attributes and methods.'''
 
+
 def lookup(obj):
     '''Returns an object's attributes and methods.'''
-    return (dir(obj))
-    
+    return (dir(obj))    
