8be5c0c7986969ea1531819165c1c1b83705b170
AKoth-Otieno <elsyakoth@gmail.com>
2022-09-29 04:11:21 -0700

    correct typo on line.

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle(__import__('7-base_geometry').BaseGeometry):
     def __init__(self, width, height):
         '''Instantiate REctangle.'''
         self.integer_validator("width", width)
-        seld().integer_validator("height", height)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
