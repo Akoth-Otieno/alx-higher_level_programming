4234cf1729218397ee0e1cf16838d3631513e06a
AKoth-Otieno <elsyakoth@gmail.com>
2022-09-29 06:52:15 -0700

    invert the == and != operators.

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''Write a class MyInt that inherints from int.'''
+
+
+class MyInt(int):
+    '''class MyInt.'''
+    def __init__(self, integer):
+        '''Instantiate MyInt and invert operators.'''
+        self.integer = integer
+
+    def __eq__(self, rebel):
+        return self.integer != integer
+
+    def __ne__(self, rebel):
+        return self.integer == integer
