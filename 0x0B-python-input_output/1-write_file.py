07992aeddb7f59ce89cfb0470f7b32b53b305ab9
AKoth-Otieno <elsyakoth@gmail.com>
2022-10-01 02:26:01 -0700

    readlines in the file.

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,8 +5,9 @@
 
 def write_file(filename="", text=""):
     '''writes a string to a text file and returns character count.'''
-    file_lines = 0
     with open(filename, mode="w", encoding="utf-8") as f:
+        file_lines = 0
         for file_line in f:
-            file_line += 1
+            file_line = f.readline()
+            file_lines += file_line
     return(file_lines)
