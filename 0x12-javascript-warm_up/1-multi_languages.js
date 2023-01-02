82eed269836e1277fea69d615af49bf712eed79f
Akoth-Otieno <ksurry5@gmailcom>
2023-01-02 15:49:41 +0000

    Write a script that prints 3 lines.

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const firstStatement = 'C is fun';
+const secondStatement = 'Python is cool';
+const lastStatement = 'Javascript is amazing';
+console.log(firstStatement);
+console.log(secondStatement);
+console.log(lastStatement);
